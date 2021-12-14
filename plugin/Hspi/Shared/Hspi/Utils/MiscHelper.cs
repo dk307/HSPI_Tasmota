902052c9147ca21258e4812e7c2cf7c25b367a83
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2021-12-13 19:43:21 -0800

    Move Files Around

---
+++ plugin/Hspi/Shared/Hspi/Utils/MiscHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+#nullable enable
+
+namespace Hspi.Utils
+{
+    internal static class MiscHelper
+    {
+        public static TValue GetValueOrDefault<TKey, TValue> (
+                this IDictionary<TKey, TValue> dictionary,
+                TKey key,
+                TValue defaultValue)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue> (
+                this ImmutableDictionary<TKey, TValue> dictionary,
+                TKey key,
+                TValue defaultValue) where TKey: notnull
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+    }
+}
